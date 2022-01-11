f2170263d4f1bb9ec220a436a608c1a62ea8af4a
Esteban Bocardo Medel <toti_irre@hotmail.com>
2022-01-10 19:12:34 -0600

    Added alert when there are no results in a search

--- src/Components/Overview.jsx
+++ src/Components/Overview.jsx
@@ -6,10 +6,12 @@ import getImage from '../Helpers/getImage';
 import formatDate from '../Helpers/formatDate';
 import Modalselectlocation from './ModalSelectLocation';
 import { searchWeatherByLocation, isEmpty } from './../Helpers/api'
+import Alert from './Alert';
 
 const Overview = (props) => {
     const [showModalSearch, setShowModalSearch] = useState(false);
     const [locations, setLocations] = useState([]);
+    const [showNotFound, setShowNotFound] = useState(false);
     const {tempUnit, setTempUnit} = useContext(TempUnitContext);
     const {activeWoeid, setActiveWoeid} = useContext(ActiveWoeidContext);
 
@@ -34,7 +36,7 @@ const Overview = (props) => {
                     setActiveWoeid(resultLocation.data[0].woeid);
                 }
             }else{
-                alert("Invalid search, insert pretty alert here :)");
+                setShowNotFound(true);
             }
         });
     }
@@ -44,6 +46,10 @@ const Overview = (props) => {
             searchAndSelectLocation(e.target.value);
         }
     }
+
+    const handleShowNotFound = (val) => {
+        setShowNotFound(val);
+    }
     
 
     return (
@@ -51,6 +57,10 @@ const Overview = (props) => {
             { showModalSearch && <Modalselectlocation locations={locations} handleLocations={(value) => {setLocations(value)}} closeModal={switchModal}/> }
             <div className='flex flex-col w-full sm:w-1/3 bg-secondary min-h-screen py-3'>
                 <div className='flex-grow'>
+                    <div className='px-3'>
+                        <Alert title="Error" content="¡Invalid search!" visible={showNotFound} close={() => {handleShowNotFound(false)}}/>
+                    </div>
+
                     <div className='flex justify-between mt-4 px-12'>
                         <input onKeyUp={handleKeyUp} className='bg-alter-gray placeholder-white px-2 shadow-md shadow-[#00000054]' type="text" placeholder='Search place...'/>
                         <button onClick={() => {searchAndSelectLocation("Mexico")}} className='bg-[#585676] rounded-full w-10 h-10 font-bold text-[18px]'><i className="material-icons text-2xl">gps_fixed</i></button>
